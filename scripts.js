333f3b0c756c7dcb4b460f8c22f8f3fd16d079fb
sarahjem <96020444+sarahjem@users.noreply.github.com>
2022-02-03 13:03:26 -0500

    animate site

---
+++ scripts.js
@@ -0,0 +1,15 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    
+    $(".hamburger-button").click(function(){
+
+        $(this).toggleClass('active');
+        $(".mobile-menu").fadeToggle();
+
+    });
+
+    // Animate on scroll - activate
+    AOS.init({
+        duration: 500
+    });
+});
