23df72e4789cb32c02e0c49b2f9ee02d2d99d15e
sarahjem <96020444+sarahjem@users.noreply.github.com>
2022-02-10 15:07:27 -0500

    gallery button

--- scripts.js
+++ scripts.js
@@ -19,3 +19,8 @@ lightGallery(document.getElementById('lightgallery'), {
     speed: 500,
     download: false
 });
+
+lightGallery(document.getElementById('lightgallery-2'), {
+    speed: 500,
+    download: false
+});
